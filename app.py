e83f9680de1569d134891408d013322c971dcf44
Declan446 <Declan.446@outlook.com>
2020-06-29 14:58:36 +0000

    added my recipes.html, gitignore and env.py files. i also pushed my mongo db to heroku and displayed it on a web page

--- app.py
+++ app.py
@@ -1,12 +1,22 @@
 import os
-from flask import Flask
+from flask import Flask, render_template, redirect, request, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
+if os.path.exists("env.py"):
+    import env
 
 app = Flask(__name__)
 
 
+app.config['MONGO_URI'] = os.environ.get("MONGO_URI")
+DATABASE = "irish_recipes"
+
+mongo = PyMongo(app)
+
 @app.route('/')
-def hello():
-    return 'Recipes Of Ireland'
+@app.route("/get_recipes")
+def get_recipes():
+    return render_template("recipes.html", recipes=mongo.db.recipes.find())
 
 
 if __name__ == '__main__':
