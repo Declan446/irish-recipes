2bc72241c640df18336fe977eadd54664d86021b
Declan446 <Declan.446@outlook.com>
2020-07-09 15:05:30 +0000

    added an edit and delete button to my recipes

--- app.py
+++ app.py
@@ -70,7 +70,7 @@ def update_recipe(recipe_id):
 @app.route("/delete_recipe/<recipe_id>")
 def delete_recipe(recipe_id):
     mongo.db.recipes.remove({"_id": ObjectId(recipe_id)})
-    return redirect(url_for("recipes2"))
+    return redirect(url_for("recipes"))
 
 
 if __name__ == '__main__':
